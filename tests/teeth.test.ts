486f7d2be35d6b3ef207985b41e0866ed47af5d2
Thea Flowers <thea@winterbloom.com>
2024-01-19 20:06:59 -0500

    WIP: switch Teeth to TypeScript

---
+++ tests/teeth.test.ts
@@ -0,0 +1,45 @@
+/*
+    Copyright (c) 2021 Alethea Katherine Flowers.
+    Published under the standard MIT License.
+    Full text available at: https://opensource.org/licenses/MIT
+*/
+
+import { assert } from "@esm-bundle/chai";
+
+import Teeth from "../src/teeth";
+
+function* generate_data(length) {
+    for (let i = 0; i < length; i++) {
+        yield i % 256;
+    }
+}
+
+function* generate_random_data(length) {
+    for (let i = 0; i < length; i++) {
+        yield Math.floor(Math.random() * 255);
+    }
+}
+
+suite("teeth", function () {
+    test("Encode/decode", function () {
+        for (let n = 1; n < 128; n++) {
+            const original = Uint8Array.from(generate_data(n));
+            const encoded = Teeth.encode(original);
+            const decoded = Teeth.decode(encoded);
+
+            assert.equal(encoded.length, Teeth.encodedLength(original));
+            assert.deepEqual(decoded, original);
+        }
+    });
+
+    test("Encode/decode (randomized)", () => {
+        for (let n = 1; n < 128; n++) {
+            const original = Uint8Array.from(generate_random_data(n));
+            const encoded = Teeth.encode(original);
+            const decoded = Teeth.decode(encoded);
+
+            assert.equal(encoded.length, Teeth.encodedLength(original));
+            assert.deepEqual(decoded, original);
+        }
+    });
+});
